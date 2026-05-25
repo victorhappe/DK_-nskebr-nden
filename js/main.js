b0c195b51c26a8f751657f5b6bc683e7a2d5bc8b
Signe <siba1000@stud.ek.dk>
2026-05-25 12:02:28 +0200

    tildigstartSigne

--- js/main.js
+++ js/main.js
@@ -7,3 +7,78 @@ function burgerClick() {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
 }
+
+/* Slider */
+
+const track = document.querySelector(".fordele-grid");
+
+const nextBtn = document.querySelector(".slider-arrow-right");
+const prevBtn = document.querySelector(".slider-arrow-left");
+
+function getStep() {
+  const card = track.firstElementChild;
+  const gap = 32;
+
+  return card.offsetWidth + gap;
+}
+
+function moveNext() {
+  const firstCard = track.firstElementChild;
+
+  track.style.transition = "transform 0.4s ease";
+  track.style.transform = `translateX(-${getStep()}px)`;
+
+  track.addEventListener("transitionend", function handler() {
+    track.appendChild(firstCard);
+
+    track.style.transition = "none";
+    track.style.transform = "translateX(0)";
+
+    track.removeEventListener("transitionend", handler);
+  });
+}
+
+function movePrev() {
+  const lastCard = track.lastElementChild;
+
+  track.insertBefore(lastCard, track.firstElementChild);
+
+  track.style.transition = "none";
+  track.style.transform = `translateX(-${getStep()}px)`;
+
+  requestAnimationFrame(() => {
+    track.style.transition = "transform 0.4s ease";
+    track.style.transform = "translateX(0)";
+  });
+}
+
+nextBtn.addEventListener("click", moveNext);
+prevBtn.addEventListener("click", movePrev);
+
+/* Modal */
+
+// få fat i modal
+var modal = document.getElementById("myModal");
+
+// få fat i knappen der åbner modal
+var btn = document.getElementById("myBtn");
+
+// få fat i <span> elementet der lukker modal
+var span = document.getElementsByClassName("close")[0];
+
+// klikker på knappen, åbner modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// Når brugeren klikker på <span> (x), lukker modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Når brugeren klikker hvor som helst uden for modal, lukker den
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
