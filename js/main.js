36b8c5dc5813660ed817ce41852c5e17c822d6ca
Signe <siba1000@stud.ek.dk>
2026-05-26 15:05:10 +0200

    disclaimersetup

--- js/main.js
+++ js/main.js
@@ -9,18 +9,12 @@ function burgerClick() {
 }
 
 /* modal disclaimer */
-document.addEventListener("DOMContentLoaded", function () {
-  const modal = document.getElementById("disclaimer-modal");
-  const btn = document.getElementById("accept-btn");
+window.addEventListener("DOMContentLoaded", () => {
+  const disclaimerModal = document.getElementById("disclaimerlogin-modal");
 
-  // Tjek om brugeren allerede har accepteret
-  if (!localStorage.getItem("disclaimerAccepted")) {
-    modal.style.display = "flex";
-  }
+  const acceptBtn = document.getElementById("accept-btn");
 
-  // Når brugeren accepterer
-  btn.addEventListener("click", function () {
-    localStorage.setItem("disclaimerAccepted", "true");
-    modal.style.display = "none";
+  acceptBtn.addEventListener("click", () => {
+    disclaimerModal.style.display = "none";
   });
 });
